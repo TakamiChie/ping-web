03ab827a49c8c8ecda58e340a66c106fa7d371c4
高見知英 <chie@onpu-tamago.net>
2023-04-26 13:14:25 +0900

    値を保存・読み込みするsavecontrol.jsを追加

---
+++ src/savecontrol.js
@@ -0,0 +1,32 @@
+window.addEventListener('DOMContentLoaded', () => {
+  Array.from(document.getElementsByClassName("savecontrols")).forEach((e) => {
+    let v = localStorage.getItem(e.id);
+    switch (e.tagName) {
+      case "INPUT":
+        switch(e.type){
+          case "checkbox":
+            e.checked = v;
+            break;
+          default:
+            e.value = v;
+            break;
+        };
+        break;
+      case "SELECT":
+        v = e.value;
+        break;
+    }
+    e.addEventListener("input", (el) => {
+      let v;
+      switch (el.target.tagName) {
+        case "INPUT":
+          v = el.target.type == "checkbox" ? el.target.checked : el.target.value;
+          break;
+        case "SELECT":
+          v = el.target.value;
+          break;
+      }
+      localStorage.setItem(el.target.id, v);
+    });
+  });
+});
